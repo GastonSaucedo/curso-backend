44f9b593d2a82074c3d8c0c732de91d3310d4eef
Gaston Saucedo <gastonsaucedo2001@gmail.com>
2024-04-16 15:49:23 -0300

    entrega Desafio 8

---
+++ Desafio-8/src/routes/products.routes.js
@@ -0,0 +1,107 @@
+import { Router } from "express";
+import { uploader, validateModifiedData } from "../../utils.js";
+import { validateFormData } from "../../utils.js";
+import ProductManager from "../services/db/products.db.service.js";
+const router = Router();
+let productManager = new ProductManager();
+router.get("/", async (req, res) => {
+  let limite = parseInt(req.query.limite);
+  limite = limite < 0 || isNaN(limite) ? false : limite;
+
+  try {
+    const productosObtenidos = await productManager.getProducts();
+
+    if (limite) {
+      return res.send(productosObtenidos.slice(0, limite));
+    } else {
+      return res.send(productosObtenidos);
+    }
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+router.get("/:id", async (req, res) => {
+  try {
+    res.send(await productManager.getProductByID(req.params.id));
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
+router.post(
+  "/",
+  uploader.single("imagen"),
+  validateFormData,
+  async (req, res) => {
+    if (req.validatedData.error) {
+      return res
+        .status(400)
+        .json({ error: JSON.parse(req.validatedData.error.message) });
+    }
+    try {
+      let imagen = [];
+      req.validatedData.data.thumb && imagen.push(req.validatedData.data.thumb);
+      await productManager.addProduct({
+        ...req.validatedData.data,
+        thumb: imagen,
+      });
+      res.redirect("/home/");
+    } catch (e) {
+      res.status(500).send(e.message);
+    }
+  }
+);
+
+router.delete("/:id", async (req, res) => {
+  try {
+    res.send(await productManager.deleteProduct(req.params.id));
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
+router.put("/:id", validateModifiedData, async (req, res) => {
+  const nuevo = req.validatedData;
+
+  if (nuevo.error) {
+    return res.status(400).json({ error: JSON.parse(nuevo.error.message) });
+  } else
+    try {
+      res.send(await productManager.updateProduct(req.params.id, nuevo.data));
+    } catch (e) {
+      res.status(400).send(e.message);
+    }
+});
+
+router.put("/:id/categoria/:cate", async (req, res) => {
+  try {
+    res.send(
+      await productManager.deleteProductCategory(req.params.id, req.params.cate)
+    );
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
+router.post(
+  "/imagenes/",
+  uploader.single("imagen"),
+  validateModifiedData,
+  async (req, res) => {
+    const id = req.body.IDproducto;
+    if (!req.file) {
+      return res
+        .status(400)
+        .send({ status: "error", mensaje: "No se adjunto archivo." });
+    }
+
+    try {
+      let imagen = [];
+      req.validatedData.data.thumb && imagen.push(req.validatedData.data.thumb);
+      res.send(await productManager.updateProduct(id, { thumb: imagen }));
+    } catch (e) {
+      res.status(500).send(e.message);
+    }
+  }
+);
+export default router;
---
+++ Desafio-8/src/services/db/carts.db.service.js
@@ -0,0 +1,85 @@
+import { cartModel } from "./models/carts.model.js";
+
+import productModel from "./models/products.model.js";
+
+class CartManager {
+  //carrito vacío
+  addCart = async () => {
+    let newCarrito = {
+      products: [],
+    };
+
+    const carrito = await cartModel.create(newCarrito);
+    console.log("carrito vacio en controlador", carrito);
+    return carrito;
+  };
+  //obtener carritos
+  getCarts = async () => {
+    const carritos = await cartModel.find().lean();
+    return carritos;
+  };
+  //obtener carrito con id determinado
+  getCartByID = async (id) => {
+    const carrito = await cartModel.findOne({ _id: id }).lean();
+    const result = carrito
+      ? carrito
+      : { success: false, message: "no se encontró ningún carrito con ese id" };
+    return result;
+  };
+  //agregar producto a un carrito específico
+  addProductToCartID = async (id, productID) => {
+    let carritoBuscado = await cartModel.findById(id);
+    if (!carritoBuscado) {
+      throw new Error("Carrito no encontrado");
+    }
+    const producto = await productModel.findById(productID); //para verificar que exista un producto con ese id
+    if (!producto) {
+      throw new Error("producto no encontrado");
+    }
+    console.log(carritoBuscado);
+    const productIndex = carritoBuscado.cart.findIndex(
+      (productItem) => productItem.product._id.toString() === productID
+    );
+
+    if (productIndex === -1) {
+      carritoBuscado.cart.push({ product: productID, qty: 1 });
+    } else {
+      carritoBuscado.cart[productIndex].qty++;
+    }
+
+    await carritoBuscado.save();
+
+    return carritoBuscado;
+  };
+
+  //borrar carrito
+  deleteFullCartByID = async (id) => {
+    const result = await cartModel.deleteOne({ _id: id });
+    return result;
+  };
+
+  //borrar producto de un carrito específico
+  deleteProductFromCart = async (id, productID) => {
+    let carritoBuscado = await cartModel.findById(id);
+    console.log(carritoBuscado);
+    if (!carritoBuscado) {
+      throw new Error("Carrito no encontrado");
+    }
+    console.log("lo que llega al controlador", id, productID);
+    const productIndex = carritoBuscado.cart.findIndex(
+      (productItem) => productItem.product._id.toString() === productID
+    );
+    console.log(productIndex);
+    if (productIndex === -1) {
+      throw new Error("el producto indicado no existe en el carrito");
+    } else {
+      carritoBuscado.cart.splice(productIndex, 1);
+    }
+    console.log("carrito buscado a guardar ", carritoBuscado);
+    await carritoBuscado.save();
+
+    return carritoBuscado;
+  };
+}
+
+export default CartManager;
